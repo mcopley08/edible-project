5c6ddfce8d1abc36c4d179f659eb6b4f1abc25ff
Michael Copley <mdcopley@Michaels-MacBook-Pro.local>
2016-05-10 16:10:52 -0400

    making changes to the development environment (grunt nodemon & watch restart, sublime live reload) & getting the structure of the webpage going

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,14 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
+	  concurrent: {
+		  dev: {
+		    tasks: ['watch', 'nodemon'],
+		    options: {
+		      logConcurrentOutput: true
+		    }
+		  }
+		},
 	  concat: {
 	    js: {
 	    	src:[
@@ -10,6 +18,7 @@ module.exports = function(grunt) {
 	    		 'public/javascript/vendor/wow.min.js',
 	    		 'public/javascript/vendor/vivus-svg-animation.min.js',
 	    		 'public/javascript/vendor/jquery.ajaxchimp.js',
+	    		 'public/javascript/assets/checkbox.js',
 	    		 'public/javascript/assets/application.js' ],
 	    	dest:'public/build/app.js'
 	    },
@@ -38,21 +47,43 @@ module.exports = function(grunt) {
 	  	dev: {
 	      script: 'index.js'
 	  	}
-	  }
+	  },
+	  watch: {
+      css: {
+        files: '<%=  concat.css.src %>',
+        tasks: ['concat','cssmin'],
+        options: {
+        	livereload: true,
+        	nonull: true
+        }
+      },
+      js: {
+        files: '<%=  concat.js.src %>',
+        tasks: ['concat','uglify'],
+        options: {
+        	livereload: true,
+        	nonull: true
+        }
+      }
+    }
 	  // Arbitrary non-task-specific properties.
 	  // my_property: 'whatever',
 	  // my_src_files: ['foo/*.js', 'bar/*.js'],
 	});
 
+	grunt.loadNpmTasks('grunt-concurrent');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-nodemon');
 
 	// this would be run by typing "grunt build" on the command line
-	grunt.registerTask('build', ['concat', 'uglify', 'cssmin', 'nodemon']);
+	grunt.registerTask('build', ['concat', 'uglify', 'cssmin']);
 
 	// the default task can be run just by typing "grunt" on the command line
-	grunt.registerTask('default', ['concat','uglify', 'cssmin', 'nodemon']);
+	grunt.registerTask('default', ['concat','uglify', 'cssmin', 'concurrent']);
+
+
 
 };
